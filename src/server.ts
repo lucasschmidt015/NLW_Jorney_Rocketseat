a403d644d607ee1288665b3255125f24c19c3dd9
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-07-09 21:21:57 -0300

    Implementado a criação de links e activities. Finalizado a implementação da confirmação dos participantes.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import fastify from 'fastify'
-import { prisma } from './lib/prisma';
 import cors from '@fastify/cors';
-import { createTrip } from './routes/create-trip';
+import fastify from 'fastify';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { confirmParticipants } from './routes/confirm-participant';
 import { confirmTrip } from './routes/confirm-trip';
+import { createTrip } from './routes/create-trip';
+import { createActivity } from './routes/create-activity';
+import { getActivities } from './routes/get-activities';
+import { createLink } from './routes/create-link';
+import { getLinks } from './routes/get-links';
 
 const app = fastify();
 
@@ -16,6 +20,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipants);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink)
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!');
